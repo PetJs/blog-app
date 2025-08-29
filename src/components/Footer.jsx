0aff34ecd3140ed9a080de3a59ad086b3017af6c
PetJs <peterfagoroye76@gmail.com>
2025-08-29 18:57:55 +0100

    feat: :sparkles: Adds Foooter component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+import {Instagram, Twitter, Github, Linkedin} from 'lucide-react';
+
+const icons = [
+    {
+        name: 'instagram',
+        icon: <Instagram size={20} />
+    },
+    {
+        name: 'twitter',
+        icon: <Twitter size={20} />
+    },
+    {
+        name: 'github',
+        icon: <Github size={20} />
+    },
+    {
+        name: 'linkedin',
+        icon: <Linkedin size={20} />
+    }
+]
+const Footer = () => {
+    return (
+        <footer className="border-t border-gray-600 p-2 mt-8">
+            <div className="flex justify-between items-center">
+                <p>&copy; 2025 Blog App. All rights reserved.</p>
+                <div className="flex gap-2">
+                    {icons.map((item) => (
+                        <Link 
+                            to={`https://${item.name}.com`}
+                            key={item.name}
+                            className="text-gray-400 hover:text-white"
+                        >
+                            {item.icon}
+                        </Link>
+                    ))}
+                </div>
+            </div>
+        </footer>
+    );
+
+}
+
+export default Footer;
