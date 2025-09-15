0620afb49c9f7dd2bd964fb7eaf3ccf80a8a1297
PetJs <peterfagoroye76@gmail.com>
2025-09-15 08:07:10 +0100

    feat: Adds Login page

---
+++ src/pages/auth/Login.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { useAuth } from "../../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const { login } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await login(email, password);
+      console.log(email, password)
+      alert("Login successful!");
+      navigate("/")
+    } catch (err) {
+      alert("Invalid credentials");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+      <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
