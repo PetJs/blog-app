662a20bba7ae28facccb807cbda8c333ec8fb6fb
Oluwamorowa <peterfagoroye76@gmail.com>
2026-06-08 16:20:53 +0100

    refactor: Update Layout component for improved structure and styling

--- src/components/Layouts/layout.jsx
+++ src/components/Layouts/layout.jsx
@@ -2,20 +2,16 @@ import { Outlet } from "react-router-dom";
 import NavBar from "../Navbar";
 import Footer from "../Footer";
 
-
 const Layout = () => {
-    return(
-        <div className="relative">
-            <NavBar />
-
-            <Outlet />
-
-            <div className="fixed bg-white bottom-0 left-0 w-full">
-                <Footer/>
-            </div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="min-h-screen flex flex-col bg-[var(--on-primary)]">
+      <NavBar />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
 export default Layout;
