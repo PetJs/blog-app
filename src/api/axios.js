748a034756feb2f2a4bc399fb902abac723778d1
Oluwamorowa <peterfagoroye76@gmail.com>
2026-06-15 20:50:59 +0100

    fix: Update API base URL to correct endpoint

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://blog-backend-1-6nvo.onrender.com", 
+  baseURL: "https://blog-backend-6ilk.onrender.com", 
 });
 
 // Attach Token
