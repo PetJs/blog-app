18c6947877731a8ed002a13acdd2cc9a73667c62
Oluwamorowa <peterfagoroye76@gmail.com>
2026-06-19 14:50:48 +0100

    fix: Correct baseURL in axios instance to include API path

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://blog-backend-6ilk.onrender.com", 
+  baseURL: "https://blog-backend-6ilk.onrender.com/api",
 });
 
 // Attach Token
